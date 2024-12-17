954fba40cabb953d1cab83a532cce63e1dab3f75
Hayato Wakasugi <Wak8810@users.noreply.github.com>
2024-12-18 00:06:43 +0900

    DFSテンプレの作成

---
+++ DFS.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+template<typename T> using vc = vector<T>;
+template<typename T> using vv = vc<vc<T>>;
+using vi = vc<int>;using vl = vc<ll>;using vb = vc<bool>;
+using vvi = vv<int>;using vvl = vv<ll>;using vvb = vv<bool>;
+
+void dfs(const vvi &G,int v,vvb &seen){
+    
+}
+
+int main(){
+    int n = 5;
+    vvi A(n);
+    vvb seen(n,false);
+}
